c5eff959c06ba4b851d35f09e821bcd4ac6007ca
Shyam Sunder Grover <post2shyam@gmail.com>
2021-03-05 14:30:28 +0530

    Home screen now shows standard header.

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import Header from "../../common/Header";
 
 class Home extends Component {
   render() {
     return (
-    <div>
-        home
-    </div>
+      <div>
+        <Header />
+      </div>
     );
   }
 }
