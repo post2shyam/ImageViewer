db55462dc26e2baf78062985b5956107a292c81d
Shyam Grover <post2shyam@gmail.com>
2021-03-05 11:37:50 +0530

    Code refactor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Login from "../src/screens/login/Login";
 import RoutingController from "../src/common/routingcontroller/RoutingController"
 import "./index.css";
 
