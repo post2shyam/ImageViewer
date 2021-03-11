19dbf37a53896f1ffb5f3d83f98a480fbfcefa88
Shyam Sunder Grover <post2shyam@gmail.com>
2021-03-11 14:21:04 +0530

    Added menu on profile pic click.

--- src/common/Header.js
+++ src/common/Header.js
@@ -2,8 +2,37 @@ import React, { Component } from "react";
 import SearchIcon from "@material-ui/icons/Search";
 import Input from "@material-ui/core/Input";
 import IconButton from "@material-ui/core/IconButton";
-
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
 import "./Header.css";
+
+const StyledMenu = withStyles({
+  paper: {
+    border: "4px",
+    backgroundColor: "#ededed",
+  },
+})((props) => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: "bottom",
+      horizontal: "left",
+    }}
+    transformOrigin={{
+      vertical: "top",
+      horizontal: "center",
+    }}
+    {...props}
+  />
+));
+
+const StyledMenuItem = withStyles((theme) => ({
+  root: {},
+}))(MenuItem);
 
 class Header extends Component {
   constructor() {
@@ -12,6 +41,16 @@ class Header extends Component {
       isLoggedIn: false,
     };
   }
+
+  //function to open the dropdown menu
+  openHandler = (e) => {
+    this.setState({ type: e.currentTarget });
+  };
+
+  //function to close the dropdown menu
+  closeHandler = () => {
+    this.setState({ type: null });
+  };
 
   render() {
     return (
@@ -27,9 +66,33 @@ class Header extends Component {
               <img
                 className="icon-button"
                 src={this.props.profile_picture}
-                alt="sj"
+                onClick={this.openHandler}
+                alt="pic"
               />
             </IconButton>
+            <StyledMenu
+              id="customized-menu"
+              anchorEl={this.state.type}
+              keepMounted
+              open={Boolean(this.state.type)}
+              onClose={this.closeHandler}
+            >
+              <StyledMenuItem>
+                <ListItemText
+                  primary={
+                    <Typography type="body2" style={{ fontWeight: "bold" }}>
+                      My Account
+                    </Typography>
+                  }
+                />
+              </StyledMenuItem>
+              <hr style={{ marginLeft: 15, marginRight: 15 }} />
+              <StyledMenuItem>
+                <Typography type="body2" style={{ fontWeight: "bold" }}>
+                  Logout
+                </Typography>
+              </StyledMenuItem>
+            </StyledMenu>
           </div>
         ) : (
           ""
