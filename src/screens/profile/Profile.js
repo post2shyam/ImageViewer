5ce4a7bda6ca58c534fd4959cb3820729865998e
Shyam Sunder Grover <post2shyam@gmail.com>
2021-03-12 17:31:40 +0530

    Profile page now redirects back to login screen if user already logged out.

--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
 import Header from "../../common/Header";
+import { useHistory } from "react-router-dom";
 import "./Profile.css";
+
+const NavigateBackToLoginPage = (props) => {
+  const history = useHistory();
+  history.push("/");
+  return "";
+};
 
 class Profile extends Component {
   constructor() {
@@ -11,15 +18,19 @@ class Profile extends Component {
   render() {
     return (
       <div>
-        <div>
-          <Header
-            isLoggedIn={true}
-            profile_picture={this.state.profilePic}
-            list={this.state.searchPost}
-            callbackFromHome={this.filteredListHandler}
-          />
-        </div>
-        <div className="container"></div>
+        {sessionStorage.getItem("access-token") !== null ? (
+          <div>
+            <Header
+              isLoggedIn={true}
+              profile_picture={this.state.profilePic}
+              list={this.state.searchPost}
+              callbackFromHome={this.filteredListHandler}
+            />
+            <div className="container"></div>
+          </div>
+        ) : (
+          <NavigateBackToLoginPage />
+        )}
       </div>
     );
   }
